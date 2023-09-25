f270d6745210ed8ee56513fb4d2d87ae7f5ff4a9
Vindi Sovia A <vindisovia@gmail.com>
2023-09-25 18:41:23 +0700

    Page Pemasukan

--- lib/pages/pemasukan.dart
+++ lib/pages/pemasukan.dart
@@ -1,68 +1,119 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:kas_bkn/main.dart';
 
-class Pemasukan extends StatelessWidget {
+class Pemasukan extends StatefulWidget {
+  @override
+  State<Pemasukan> createState() => _PemasukanState();
+}
+
+class _PemasukanState extends State<Pemasukan> {
+  TextEditingController _dateController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Tambah Pemasukan"),
+        title: Text("Pemasukan"),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Nominal:"),
-          TextField(),
-          SizedBox(
-            height: 10,
-          ),
-          Text("Keterangan:"),
-          TextField(),
-          SizedBox(
-            height: 10,
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.orange, // Warna merah
-                  ),
-                  child: Text(
-                    "Reset",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.green, // Warna kuning
-                  ),
-                  child: Text(
-                    "Simpan",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/beranda');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blue, // Warna merah
-                  ),
-                  child: Text("<< Kembali"),
-                ),
-                SizedBox(width: 10),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Tambah Pemasukan",
+              style: TextStyle(color: Colors.green, fontSize: 20),
             ),
-          )
-        ],
+            Text(''),
+            Text(
+              "Tanggal:",
+              style: TextStyle(fontSize: 18),
+            ),
+            TextField(
+              controller: _dateController,
+              decoration: InputDecoration(
+                filled: true,
+                prefixIcon: Icon(Icons.calendar_today),
+                enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue)),
+              ),
+              readOnly: true,
+              onTap: () {
+                _selectDate();
+              },
+            ),
+            Text(
+              "Nominal:",
+              style: TextStyle(fontSize: 18),
+            ),
+            TextField(),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Keterangan:",
+              style: TextStyle(fontSize: 18),
+            ),
+            TextField(),
+            SizedBox(
+              height: 10,
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.orange, // Warna merah
+                    ),
+                    child: Text(
+                      "Reset",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green, // Warna kuning
+                    ),
+                    child: Text(
+                      "Simpan",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/beranda');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue, // Warna merah
+                    ),
+                    child: Text("<< Kembali"),
+                  ),
+                  SizedBox(width: 10),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> _selectDate() async {
+    DateTime? _picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2000),
+        lastDate: DateTime(3000));
+
+    if (_picked != null) {
+      setState(() {
+        _dateController.text = _picked.toString().split(" ")[0];
+      });
+    }
   }
 }
